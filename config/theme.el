f3bedb7b2844038fce2fade68f56b21f5c9a79d9
Zachary Elliott <zach@nyu.edu>
2013-04-25 14:55:16 -0400

    Updaing SHA256 sums

--- config/theme.el
+++ config/theme.el
@@ -28,7 +28,7 @@
 ;; SHA256 for "safe" theme load
 (dolist (hash '(
   ;; birds-of-paradise-plus-theme.el
-  "8281168b824a806489ca7d22e60bb15020bf6eecd64c25088c85b3fd806fc341"
+  "0a4879589b651a17e5a1f54d4fecc306fb8631a138d0e5f8585256189fb6740a"
 
   ;; color-theme-solarized.el
   "1a268a25c38c13b33e527c79a9fea89e6e0dde44dec24e1f23a4ec813ed5a288"
@@ -43,8 +43,10 @@
   "bbe0b1af952694c3b9ba146c073adcc19c11f6b1c06d1bc34f983b31591628db"
 
   ;; zenburn-theme.el
-  "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29"))
-	(add-to-list 'custom-safe-themes hash))
+  "52b5da0a421b020e2d3429f1d4929089d18a56e8e43fe7470af2cea5a6c96443"
+  ))
+  (add-to-list 'custom-safe-themes hash))
+
 
 (load-theme 'wombat)
 
