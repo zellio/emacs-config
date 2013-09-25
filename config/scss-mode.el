8e940f605b447f52d5f18a7421397b67d3bb1f32
Zachary Elliott <zach@nyu.edu>
2013-09-25 18:26:35 -0400

    Adding configuration for scss mode

---
+++ config/scss-mode.el
@@ -0,0 +1,17 @@
+
+;;; scss-mode.el --- scss-mode-mode Configuration
+
+;; Copyright (C) 2013 Zachary Elliott
+;; See LICENSE for more information
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+(setq scss-compile-at-save nil)
+
+;;; Code:
+
+;; end of scss-mode.el
