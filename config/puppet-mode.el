44d3f415818138490a15117786af70e2fc9706d1
Zachary Elliott <zach@nyu.edu>
2013-11-08 14:05:48 -0500

    Adding puppet-mode config file  - update to auto-mode alist

---
+++ config/puppet-mode.el
@@ -0,0 +1,17 @@
+
+;;; puppet-mode.el --- puppet-mode Configurations
+
+;; Copyright (C) 2012,2013 Zachary Elliott
+;; See LICENSE for more information
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
+
+;; end of puppet.el
