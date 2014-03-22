d29a10f1a5bf86272400822368b30fe0f1692a01
Zachary Elliott <zach@nyu.edu>
2014-03-22 03:18:38 -0400

    Adding `null' safe check for shutting stuff off

--- config/emacs/global.el
+++ config/emacs/global.el
@@ -67,8 +67,9 @@
 
 (pending-delete-mode t)
 (show-paren-mode     t)
-(tool-bar-mode       0)
-(scroll-bar-mode    -1)
-(menu-bar-mode      -1)
+
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; end of global.el
