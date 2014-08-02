7983fce5107d5f59d037d33c2130d6efb0770317
Zachary Elliott <zach@nyu.edu>
2014-08-01 21:59:26 -0400

    Updating tramp config

--- config/emacs/tramp.el
+++ config/emacs/tramp.el
@@ -12,7 +12,15 @@
 
 ;;; Code:
 
+(require 'tramp)
+
 (setq
  tramp-default-method "ssh"
  tramp-persistency-file-name
  (expand-file-name "tramp-connection-history" user:emacs-data-directory))
+
+(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+
+
+;; end of tramp.el
