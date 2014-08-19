353edd04b3383e75fe1fe78971a17001dc91592a
Zachary Elliott <zach@nyu.edu>
2014-08-19 17:31:04 -0400

    Adding clojure config

---
+++ config/pkgs/clojure.el
@@ -0,0 +1,17 @@
+
+;;; clojure.el --- CLOJURE configuration
+
+;; Copyright (C) 2012-2014 Zachary Elliott
+;; See LICENSE for more information
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
+
+;; end of clojure.el
