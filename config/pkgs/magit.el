dbaa202a33ef8cbd594038c731a849d0a111c5cb
Zachary Elliott <zach@nyu.edu>
2014-08-22 00:26:10 -0400

    Adding magit commands to "C-c m ..."

---
+++ config/pkgs/magit.el
@@ -0,0 +1,18 @@
+
+;;; magit.el --- magit Configuration
+
+;; Copyright (C) 2012-2014 Zachary Elliott
+;; See LICENSE for more information
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(global-set-key (kbd "C-c m s") 'magit-status)
+(global-set-key (kbd "C-c m b") 'magit-blame-mode)
+
+;;; magit.el ends here
