73f26030e03c808a2a554a0b3e6116e4570fc2b4
Zachary Elliott <zach@nyu.edu>
2014-12-03 23:56:18 -0500

    Adding shell php mode config

---
+++ config/pkgs/php.el
@@ -0,0 +1,16 @@
+
+;;; php.el --- php Configuration
+
+;; Copyright (C) 2012-2014 Zachary Elliott
+;; See LICENSE for more information
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+;;; php.el ends here
