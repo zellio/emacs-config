8d8ccf967baa56c6484f011a3db52420300ddb8d
Zachary Elliott <contact@zell.io>
2016-09-24 09:38:29 -0400

    Switch to dracula theme

--- config/emacs/theme.el
+++ config/emacs/theme.el
@@ -11,6 +11,12 @@
 
 ;;; Code:
 
-(load-theme 'wombat)
+(add-to-list 'custom-theme-load-path package-user-dir)
+
+(setq
+ custom-safe-themes
+ '("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55"))
+
+(load-theme 'dracula)
 
 ;;; config/emacs/theme.el ends here
--- init.el
+++ init.el
@@ -6,7 +6,6 @@
 (load "emacs/defuns")
 (load "emacs/utf-8")
 (load "emacs/frames")
-(load "emacs/theme")
 (load "emacs/tabs")
 (load "emacs/scratch")
 (load "emacs/save-place")
@@ -29,3 +28,20 @@
 (load "pkg/magit")
 (load "pkg/markdown")
 (load "pkg/notmuch")
+
+(load "emacs/theme")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+	("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cperl-array-face ((t (:foreground "green" :weight bold))))
+ '(cperl-hash-face ((t (:foreground "purple" :weight bold :slant italic)))))
