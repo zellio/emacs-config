7d7c219928bb704c57322bffcb5d94f1f9a2c8df
Zachary Elliott <contact@zell.io>
2018-05-28 20:31:14 -0400

    Add rust configs

---
+++ config/pkg/rust.el
@@ -0,0 +1,14 @@
+;;; config/pkg/rust.el --- rust-mode configuration
+
+;; Copyright (C) 2012-2016 Zachary Elliott
+;; See LICENSE for more information
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;;; config/pkg/rust.el ends here
