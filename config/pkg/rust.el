f3b7420e568059ada6d1b44f7674336a3ccf4a67
Zachary Elliott <contact@zell.io>
2018-05-28 21:02:36 -0400

    Update rust configuration

--- config/pkg/rust.el
+++ config/pkg/rust.el
@@ -11,4 +11,13 @@
 
 ;;; Code:
 
+(require 'rust-mode)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
 ;;; config/pkg/rust.el ends here
