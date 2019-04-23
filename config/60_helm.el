d410161fb4134ca716d8609a0a722242e3b26430
Zachary Elliott <contact@zell.io>
2019-04-23 14:28:58 -0400

    Sorts helm bindings

--- config/60_helm.el
+++ config/60_helm.el
@@ -23,9 +23,16 @@
 
 ;;; Global-map
 (global-set-key [remap execute-extended-command] 'helm-M-x)
+(global-set-key [remap find-file] 'helm-find-files)
+(global-set-key [remap dabbrev-expand] 'helm-dabbrev)
+(global-set-key [remap find-tag] 'helm-etags-select)
+(global-set-key [remap jump-to-register] 'helm-register)
+(global-set-key [remap list-buffers] 'helm-buffers-list)
+(global-set-key [remap occur] 'helm-occur)
+(global-set-key [remap xref-find-definitions] 'helm-etags-select)
+
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-c f") 'helm-recentf)
-(global-set-key [remap find-file] 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-c <SPC>") 'helm-all-mark-rings)
 (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
@@ -36,13 +43,8 @@
 (global-set-key (kbd "C-x C-d") 'helm-browse-project)
 (global-set-key (kbd "<f1>") 'helm-resume)
 (global-set-key (kbd "C-h C-f") 'helm-apropos)
-(global-set-key [remap dabbrev-expand] 'helm-dabbrev)
-(global-set-key [remap find-tag] 'helm-etags-select)
-(global-set-key [remap jump-to-register] 'helm-register)
-(global-set-key [remap list-buffers] 'helm-buffers-list)
-(global-set-key [remap occur] 'helm-occur)
-(global-set-key [remap xref-find-definitions] 'helm-etags-select)
-;; (global-set-key (kbd "C-c i") 'helm-imenu-in-all-buffers)
+(global-set-key (kbd "C-c i") 'helm-imenu-in-all-buffers)
+
 ;; (define-key shell-mode-map (kbd "M-p") 'helm-comint-input-ring)
 
 (helm-mode 1)
