0ce78a0cb93746ce0b43d6ca4c1dfe48ed48a653
Zachary Elliott <contact@zell.io>
2024-02-22 09:59:00 -0500

    Cleans up org-mode config

--- config/70_org.el
+++ config/70_org.el
@@ -24,25 +24,54 @@
   :group 'user)
 
 (defcustom user/org-default-notes-file
-  (expand-file-name "notest.org" user/org-directory)
+  (expand-file-name "notes.org" user/org-directory)
   ""
   :type 'string
   :group 'user)
 
 (use-package org
   :mode (("\\.org$" . org-mode))
+
+  :preface
+  (defun user/org-capture-template-todo ()
+    (let ((lines '("* TODO %^{Description}%? %^g" ":LOGBOOK:"
+                   "- Created                              %U"
+                   ":END:")))
+    (mapconcat 'identity lines "\n  ")))
+
+  (defun user/org-capture-template-work-task ()
+    (let ((lines '("* TODO %^{Type|CORE|SRE|DEVX|POMO}-%^{Ticket number} - %^{Description}%?"
+                   ":PROPERTIES:"
+                   ":LINK:     https://formationbio.atlassian.net/browse/%\\1-%\\2"
+                   ":END:"
+                   ":LOGBOOK:"
+                   "- Created                              %U"
+                   ":END:")))
+      (mapconcat 'identity lines "\n  ")))
+
+  (defun user/org-capture-template-journal ()
+    (let ((lines '("* %^{Description}"
+                   ":LOGBOOK:"
+                   "- Captured %U"
+                   ":END:"
+                   "%?")))
+    (mapconcat 'identity lines "\n  ")))
+
   :general
   ("C-c o a" 'org-agenda)
   ("C-c o r" 'org-capture)
+
   :hook (org-mode . (lambda ()
                       (local-set-key [(control return)] 'org-insert-heading-after-current)))
+
   :config
   (setq
    org-directory user/org-directory
    org-default-notes-file user/org-default-notes-file
    org-agenda-files (list user/org-directory)
    org-M-RET-may-split-line nil
-   org-log-done t
+   org-log-done nil
+   org-log-into-drawer t
    org-use-speed-commands t
    org-id-track-globally t
    org-highest-priority ?A
@@ -50,7 +79,7 @@
    org-lowest-priority ?Z
 
    org-todo-keywords
-   '((sequence "TODO(t)" "INPR(i)" "|" "DONE(d)")
+   '((sequence "TODO(t)" "INPR(i!)" "|" "DONE(d@)")
      (sequence "WAIT(w@/!)" "HOLD(h@/!)" "|" "OVER(c@/!)" "PHONE" "MEETING")
      (sequence "TODO" "INPR" "REVW" "TEST" "|" "DONE" "ARCH" "WONT"))
 
@@ -71,14 +100,22 @@
      ("DONE" ("WAIT") ("OVER") ("HOLD")))
 
    org-capture-templates
-   '(("t" "TODO"
+   '(("t" "Todo"
       entry (file org-default-notes-file)
-      "\n* TODO %?\n"
-      :empty-lines 1)
+      (function user/org-capture-template-todo)
+      :empty-lines-after 1
+      :prepend t)
+
+     ("w" "Work Todo"
+      entry (file org-default-notes-file)
+      (function user/org-capture-template-work-task)
+      :empty-lines-after 1
+      :prepend t)
 
      ("j" "Journal"
       entry (file+datetree user/org-journal-file)
-      "* %<%H:%M> - %?"
-      :empty-lines 1))))
+      (function user/org-capture-template-journal)
+      :empty-lines-before 1))
+   ))
 
 ;;; 70_org.el ends here
