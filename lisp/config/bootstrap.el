cc97b07a3aa5bdebc68eedf332d85cab50346148
Zachary Elliott <contact@zell.io>
2026-05-23 12:18:48 -0400

    Updates elpaca version

--- lisp/config/bootstrap.el
+++ lisp/config/bootstrap.el
@@ -29,64 +29,64 @@
 
 ;;; Code:
 
-;; Install elpaca
+;;; Elpaca bootstrap
 
-(declare-function elpaca "elpaca")
 (declare-function elpaca-generate-autoloads "elpaca")
 (declare-function elpaca-process-queues "elpaca")
+(declare-function elpaca "elpaca")
 (declare-function elpaca-use-package-mode "elpaca-use-package")
 
-(defvar elpaca-installer-version 0.11)
+(defvar elpaca-installer-version 0.12)
 (defvar elpaca-directory (expand-file-name "var/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order
-  '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-           :ref nil :depth 1 :inherit ignore
-           :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-           :build (:not elpaca--activate-package)))
-(let* ((repo (expand-file-name "elpaca/" elpaca-repos-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca-activate)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
-  (push (if (file-exists-p build) build repo) load-path)
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                  (zerop
-                   (call-process
-                    "git" nil buffer t "clone" (format "--depth=%d" (plist-get order :depth))
-                    "--no-single-branch" (plist-get order :repo) repo))
-                  (zerop
-                   (call-process
-                    "git" nil buffer t "checkout" (or (plist-get order :ref) "--")))
-                  (emacs (expand-file-name invocation-name invocation-directory))
-                  ((zerop
-                    (call-process
-                     emacs nil buffer nil "-Q" "-L" "." "--batch"
-                     "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
                   ((require 'elpaca))
                   ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn
-              (message "%s" (buffer-string))
-              (kill-buffer buffer))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
           (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (let* ((load-source-file-function nil))
-      (load "./elpaca-autoloads"))))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 
 (add-hook 'after-init-hook #'elpaca-process-queues)
+
+;; Install elpaca
 
 (elpaca `(,@elpaca-order))
 
 (elpaca elpaca-use-package
   (elpaca-use-package-mode))
 
-;; Install minimal package set
+;; Install minimum package set
+
+(use-package compat
+  :ensure (:wait t)
+  :demand t)
 
 (use-package no-littering
   :ensure (:wait t)
